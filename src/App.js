a87e921d56f5b582c9a445fa65da0d6098da5bc1
AlexandrRzz <rzz400@gmail.com>
2020-12-07 15:44:57 +0200

    remove router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Employees from "./components/employees/employees";
-import Home from "./components/home/home";
 
 function App() {
   return (
-    <Router>
-      <div className="App"></div>
-      <Switch>
-        <Route exact path="/employees" component={Employees} />
-        <Route path="/" component={Home} />
-      </Switch>
-    </Router>
+    <div className="App">
+      <Employees />
+    </div>
   );
 }
 
